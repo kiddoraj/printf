a296e37e36ac7cd137d0f2d97c8803a7163ec578
Gmwongela <gvidla73@gmail.com>
2023-04-24 19:33:47 +0000

    updated

---
+++ printf_rot13.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * printf_rot13 - function of rot13 translation of string
+ * @args: type struct va_arg which is allocated to printf arguments
+ * Return: counter
+ *
+ */
+
+int printf_rot13(va_list args)
+{
+	int i, j, counter = 0;
+	int k = 0;
+	char *s = va_arg(args, char*);
+	char alpha[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char beta[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i]; i++)
+	{
+		k = 0;
+		for (j = 0; alpha[j] && !k; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				_putchar(beta[j]);
+				counter++;
+				k = 1;
+			}
+		}
+		if (!k)
+		{
+			_putchar(s[i]);
+			counter++;
+		}
+	}
+	return (counter);
+}
