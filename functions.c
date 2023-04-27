eec6f5cdbf45c8d1381f1b7860963c99202b23b8
rajy <kiddo.rajy@gmail.com>
2023-04-27 11:52:35 -0700

    handles

--- functions.c
+++ functions.c
@@ -122,7 +122,7 @@ int print_int(va_list types, char buffer[],
 	n = convert_size_number(n, size);
 
 	if (n == 0)
-		buffer[i--] = '0';
+		buffer[j--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 	num = (unsigned long int)n;
