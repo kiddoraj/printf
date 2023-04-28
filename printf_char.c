b13ba6219ee0a19dc5ad39fa047cd913c9667879
rajy <kiddo.rajy@gmail.com>
2023-04-28 13:18:38 -0700

    final review

---
+++ printf_char.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * printf_char - prints a char.
+ * @val:args
+ *
+ * Return: 1.
+ */
+int printf_char(va_list val)
+{
+	char s;
+
+	s = va_arg(val, int);
+
+	_putchar(s);
+	return (1);
+}
---
+++ printf_str.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _strlen - Returns the length of a string.
+ * @s:pointer
+ *
+ * Return: c.
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+		;
+	return (c);
+
+}
+/**
+ * _strlenc - str length const
+ * @s: Type char pointer
+ * Return: c
+ */
+int _strlenc(const char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+		;
+	return (c);
+}
